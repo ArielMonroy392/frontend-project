f58d2f7bde89a5c0787272748760a1ff32024942
Ariel Monroy <ariel.monroy@jala.university>
2025-06-02 15:10:34 -0400

    fix: update PokemonTitle component to use pokemon.id instead of pokemon.number

--- src/components/molecules/PokemonTitle.jsx
+++ src/components/molecules/PokemonTitle.jsx
@@ -4,7 +4,7 @@ import './PokemonTitle.css';
 const PokemonTitle = ({ pokemon }) => {
     return (
         <div className="pokemon-info font-bold">
-            <Text className="text-large">#{pokemon.number}</Text>
+            <Text className="text-large">{pokemon.id}</Text>
             <Text className="capitalize text-xxl font-bold">{pokemon.name}</Text>
         </div>
     )
