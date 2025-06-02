d94609276458be34781ed7672dcebb727a45746c
Ariel Monroy <ariel.monroy@jala.university>
2025-06-02 15:11:02 -0400

    feat: implement Pokemon fetching logic and update rendering in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import PokemonCard from './components/organisms/pokemonCard'
 
-const pokemon = {
-  name: 'Bulbasaur',
-  number: '001',
-  types: ['grass', 'poison'],
-  image: '01.png',
-}
-
 function App() {
+  const [pokemons, setPokemons] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+        const data = await response.json()
+        const { results } = data
+        const mappedPokemons = await Promise.all(results.map(async (pokemon) => {
+          const newPokemon = await fetch(pokemon.url)
+          return newPokemon.json()
+        }))
+        console.log('mappedPokemons', mappedPokemons)
+        setPokemons(mappedPokemons)
+
+      } catch (error) {
+        console.error('Error fetching pokemons:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPokemons()
+  }, [])
+
+
   return (
     <div>
-      <PokemonCard pokemon={pokemon} />
+      {
+        loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul className="pokemon-list">
+            {
+              pokemons.map((pokemon) => (
+                <li key={pokemon.id}>
+                  <PokemonCard pokemon={pokemon} />
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
     </div>
   )
 }
