cabb4b93f03d250088a151e977b49d7f1e2a2a78
Ariel Monroy <ariel.monroy@jala.university>
2025-06-02 15:10:26 -0400

    fix: correct type usage in TypeBadge component for proper rendering

--- src/components/molecules/typeBadge.jsx
+++ src/components/molecules/typeBadge.jsx
@@ -1,12 +1,12 @@
 import Icon from "../atoms/icon";
 import Text from "../atoms/Text";
-import './TypeBadge.css'; // Assuming you have a CSS file for styling
+import './TypeBadge.css';
 
 const TypeBadge = ({ type }) => {
     return (
-        <span className={`badge ${type}`}>
-            <Icon type={type} />
-            <Text className="text-small capitalize font-medium">{type}</Text>
+        <span className={`badge ${type.type.name}`}>
+            <Icon type={type.type.name} />
+            <Text className="text-small capitalize font-medium">{type.type.name}</Text>
         </span>
     )
 }
