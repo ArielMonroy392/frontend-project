b7e4bd234d9dea202e0b35e129fd2e9cd6e8317a
Ariel Monroy <ariel.monroy@jala.university>
2025-06-23 14:38:38 -0400

    feat: add SVG assets for dots and pokeball backgrounds, update PokemonCard layout

--- src/components/organisms/pokemonCard.jsx
+++ src/components/organisms/pokemonCard.jsx
@@ -1,33 +1,36 @@
 import PokemonImage from "../atoms/PokemonImage";
 import PokemonTitle from "../molecules/PokemonTitle";
 import TypeBadge from "../molecules/typeBadge";
-import './PokemonCard.css';
+import "./PokemonCard.css";
 
 const PokemonCard = ({ pokemon }) => {
-    return (
-        <article className={`card bg-${pokemon.types[0].type.name}`}>
-            <div className="card-info">
-                <PokemonTitle pokemon={pokemon} />
-                <ul>
-                    {
-                        pokemon.types.map((type, index) => (
-                            <li key={index}>
-                                <TypeBadge className={`type ${type}`} type={type}>{type}</TypeBadge>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
+  return (
+    <article className={`card bg-${pokemon.types[0].type.name}`}>
+      <div className="bg">
+        <img src="/dots.svg" alt="Dots" className="dots" />
+        <img src="/pokeball-bg.svg" alt="Pokeball" className="pokeball" />
+      </div>
+      <div className="card-info">
+        <PokemonTitle pokemon={pokemon} />
+        <ul>
+          {pokemon.types.map((type, index) => (
+            <li key={index}>
+              <TypeBadge className={`type ${type}`} type={type}>
+                {type}
+              </TypeBadge>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-            <div className="card-image">
-                <PokemonImage
-                    src={pokemon.sprites.other['official-artwork'].front_default}
-                    alt={pokemon.name}
-                />
-            </div>
-
-        </article>
-    )
-}
+      <div className="card-image">
+        <PokemonImage
+          src={pokemon.sprites.other["official-artwork"].front_default}
+          alt={pokemon.name}
+        />
+      </div>
+    </article>
+  );
+};
 
 export default PokemonCard;
