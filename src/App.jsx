f5c9b33da3a65c62613ea9e7b57a622fd7065f09
Ariel Monroy <ariel.monroy@jala.university>
2025-06-23 14:39:20 -0400

    feat: refactor App component to use Layout and PokemonList, remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,15 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
-import PokemonCard from './components/organisms/pokemonCard'
+import PokemonList from './components/organisms/PokemonList'
+import Layout from './components/templates/Layout'
 
 function App() {
-  const [pokemons, setPokemons] = useState([])
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    const fetchPokemons = async () => {
-      setLoading(true)
-      try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=12')
-        const data = await response.json()
-        const { results } = data
-        const mappedPokemons = await Promise.all(results.map(async (pokemon) => {
-          const newPokemon = await fetch(pokemon.url)
-          return newPokemon.json()
-        }))
-        console.log('mappedPokemons', mappedPokemons)
-        setPokemons(mappedPokemons)
-
-      } catch (error) {
-        console.error('Error fetching pokemons:', error)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchPokemons()
-  }, [])
-
+  
 
   return (
-    <div>
-      {
-        loading ? (
-          <p>Loading...</p>
-        ) : (
-          <ul className="pokemon-list">
-            {
-              pokemons.map((pokemon) => (
-                <li key={pokemon.id}>
-                  <PokemonCard pokemon={pokemon} />
-                </li>
-              ))
-            }
-          </ul>
-        )
-      }
-    </div>
+    <Layout>
+      <PokemonList/>
+    </Layout>
   )
 }
 
