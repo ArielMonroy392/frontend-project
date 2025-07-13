74dff29ca37b560e4d7ceeb49e69593018033bc6
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 17:16:57 -0400

    refactor: rename CSS class from 'pokeball' to 'pokeball-loader' for consistency

--- src/components/atoms/PokeballLoader.jsx
+++ src/components/atoms/PokeballLoader.jsx
@@ -3,7 +3,7 @@ import './PokeballLoader.css';
 export default function PokemonLoader() {
   return (
     <div className="wrapper">
-      <div className="pokeball">
+      <div className="pokeball-loader">
       </div>
     </div>
   )
