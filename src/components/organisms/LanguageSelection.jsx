ca1fa569e219262b3cfff378520aa4862366c530
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 16:44:13 -0400

    fix: replace languages prop with SUPPORTED_LANGUAGES constant in LanguageSelection component

--- src/components/organisms/LanguageSelection.jsx
+++ src/components/organisms/LanguageSelection.jsx
@@ -1,13 +1,14 @@
 import FlagButton from "../atoms/FlagButton";
 import './LanguageSelection.css';
 import Text from "../atoms/Text";
+import { SUPPORTED_LANGUAGES } from "../../constants/game";
 
-export default function LanguageSelection({ languages, onLanguageChange }) {
+export default function LanguageSelection({ onLanguageChange }) {
   return (
     <div className="language-selection">
       <Text className={"text-large text-white font-bold"}>Select Language</Text>
       <ul className="language-list">
-        {languages.map((language) => (
+        {SUPPORTED_LANGUAGES.map((language) => (
           <li key={language}>
             <FlagButton
               language={language}
