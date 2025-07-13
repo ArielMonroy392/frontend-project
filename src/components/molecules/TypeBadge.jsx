fcda69bc2bfe5a5a355f4abb5b900813db740c74
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 17:16:50 -0400

    feat: add TypeBadge component for displaying type information

--- src/components/molecules/TypeBadge.jsx
+++ src/components/molecules/TypeBadge.jsx
@@ -1,11 +1,11 @@
-import Icon from "../atoms/icon";
+import Icon from "../atoms/Icon";
 import Text from "../atoms/Text";
 import './TypeBadge.css';
 
 const TypeBadge = ({ type }) => {
     return (
         <span className={`badge ${type.type.name}`}>
-            <Icon type={type.type.name} />
+            <Icon icon={`${type.type.name}`} />
             <Text className="text-small capitalize font-medium">{type.type.name}</Text>
         </span>
     )
