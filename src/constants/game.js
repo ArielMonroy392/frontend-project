e71f8187de941f15019a2239d908def57428dc33
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 17:32:57 -0400

    fix: update MAX_TRIES constant to increase the number of attempts

--- src/constants/game.js
+++ src/constants/game.js
@@ -1,4 +1,4 @@
-export const MAX_TRIES = 3;
+export const MAX_TRIES = 5;
 export const SUPPORTED_LANGUAGES = ["ja", "en", "fr", "de", "es", "it", "ko", "zh-Hans", "zh-Hant"];
 export const API_BASE_URL = "https://pokeapi.co/api/v2/pokemon-species";
 export const API_SPRITE_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork";
