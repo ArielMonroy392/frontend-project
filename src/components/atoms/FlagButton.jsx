92fd1d8e90b873cefa5c836555fe9e44976f01e5
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 16:40:41 -0400

    feat: simplify FlagButton by removing country code mapping and using local flag images

--- src/components/atoms/FlagButton.jsx
+++ src/components/atoms/FlagButton.jsx
@@ -1,23 +1,9 @@
-const countryCode = new Map([
-  ["ja-Hrkt", "JP"],
-  ["roomaji", "JP"],
-  ["ko", "KR"],
-  ["zh-Hant", "TW"],
-  ["fr", "FR"],
-  ["de", "DE"],
-  ["es", "ES"],
-  ["it", "IT"],
-  ["en", "US"],
-  ["ja", "JP"],
-  ["zh-Hans", "CN"]
-]);
-
 import './FlagButton.css';
 
 export default function FlagButton({ language, onClick }) {
   return (
     <button className="flag-button" onClick={() => onClick(language)}>
-      <img src={`https://flagsapi.com/${countryCode.get(language)}/flat/64.png`} alt={`${language} flag`} />
+      <img src={`flags/${language}.png`} alt={`${language} flag`} />
     </button>
   );
 }
