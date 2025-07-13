84edd3516ce3e824a38ea20b008c6c25996055e9
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 16:44:09 -0400

    fix: update MAX_TRIES reference in Game component for consistency

--- src/components/organisms/Game.jsx
+++ src/components/organisms/Game.jsx
@@ -2,9 +2,10 @@ import PokemonImage from "../atoms/PokemonImage";
 import Button from "../atoms/Button";
 import "./Game.css";
 import PokemonLoader from "../atoms/PokeballLoader";
+import { MAX_TRIES } from "../../constants/game";
 
 export default function Game({ isLoading, randomPoke, hiddenPoke, isFinished, onSelectPokemon, language, tries, onNextTry }) {
-
+  console.log(randomPoke);
   return (
     <div className="game-container">
       {isLoading ? (
@@ -25,6 +26,7 @@ export default function Game({ isLoading, randomPoke, hiddenPoke, isFinished, on
                 <Button
                   variant="primary"
                   onClick={() => onSelectPokemon(pokemon)}
+                  disabled={isFinished || tries >= MAX_TRIES}
                 >
                   {pokemon.name.get(language)}
                 </Button>
@@ -32,7 +34,7 @@ export default function Game({ isLoading, randomPoke, hiddenPoke, isFinished, on
             ))}
           </ul>}
 
-          {isFinished && tries < 3 && (
+          {isFinished && tries < MAX_TRIES && (
             <Button variant="secondary" onClick={onNextTry}> Next pokemon </Button>
           )}
         </>
