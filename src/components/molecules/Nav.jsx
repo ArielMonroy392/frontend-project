0463af64786a1390b753f3378ff480e2904f9fe1
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 17:18:14 -0400

    fix: replace anchor tags with Link components for navigation consistency

--- src/components/molecules/Nav.jsx
+++ src/components/molecules/Nav.jsx
@@ -1,5 +1,6 @@
 import "./Nav.css";
 import Text from "../atoms/Text";
+import { Link } from "react-router";
 
 export default function Nav() {
   return (
@@ -9,9 +10,9 @@ export default function Nav() {
         <Text className="nav-title">Pokédex</Text>
       </div>
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/game">Game</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   )
