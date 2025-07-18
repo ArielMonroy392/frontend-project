235e89928c0cbf6271a543ded028f2ab490716a4
Ariel Monroy <ariel.monroy@jala.university>
2025-07-18 16:36:42 -0400

    refactor: standardize import formatting and improve Toaster component usage

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,26 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import Layout from './components/templates/Layout.jsx'
-import WhosThatPokemon from './components/templates/WhosThatPokemon.jsx'
-import List from './components/templates/List.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Layout from "./components/templates/Layout.jsx";
+import WhosThatPokemon from "./components/templates/WhosThatPokemon.jsx";
+import List from "./components/templates/List.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     Component: Layout,
     children: [
       { index: true, Component: WhosThatPokemon },
-      { path: 'list', Component: List }
-    ]
-  }
-])
+      { path: "list", Component: List },
+    ],
+  },
+]);
 
-import { Toaster } from 'sonner'
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <>
-    <Toaster />
+    <Toaster richColors />
     <RouterProvider router={router} />
-
   </>
-)
+);
