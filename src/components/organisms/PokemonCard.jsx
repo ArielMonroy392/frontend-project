7cb61a6501b1d040878870dcd712f2de58f41153
Ariel Monroy <ariel.monroy@jala.university>
2025-07-19 11:12:13 -0400

    feat: add Link to base card for navigation

--- src/components/organisms/PokemonCard.jsx
+++ src/components/organisms/PokemonCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import PokemonImage from "../atoms/PokemonImage";
 import PokemonTitle from "../molecules/PokemonTitle";
 import TypeBadge from "../molecules/TypeBadge";
@@ -6,29 +7,31 @@ import "./PokemonCard.css";
 const PokemonCard = ({ pokemon }) => {
   return (
     <article className={`card bg-${pokemon.types[0].type.name}`}>
-      <div className="bg">
-        <img src="/dots.svg" alt="Dots" className="dots" />
-        <img src="/pokeball-bg.svg" alt="Pokeball" className="pokeball" />
-      </div>
-      <div className="card-info">
-        <PokemonTitle pokemon={pokemon} />
-        <ul>
-          {pokemon.types.map((type, index) => (
-            <li key={index}>
-              <TypeBadge className={`type ${type}`} type={type}>
-                {type}
-              </TypeBadge>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Link to={`/pokemon/${pokemon.id}`}>
+        <div className="bg">
+          <img src="/dots.svg" alt="Dots" className="dots" />
+          <img src="/pokeball-bg.svg" alt="Pokeball" className="pokeball" />
+        </div>
+        <div className="card-info">
+          <PokemonTitle pokemon={pokemon} />
+          <ul>
+            {pokemon.types.map((type, index) => (
+              <li key={index}>
+                <TypeBadge className={`type ${type}`} type={type}>
+                  {type}
+                </TypeBadge>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className="card-image">
-        <PokemonImage
-          src={pokemon.sprites.other["official-artwork"].front_default}
-          alt={pokemon.name}
-        />
-      </div>
+        <div className="card-image">
+          <PokemonImage
+            src={pokemon.sprites.other["official-artwork"].front_default}
+            alt={pokemon.name}
+          />
+        </div>
+      </Link>
     </article>
   );
 };
