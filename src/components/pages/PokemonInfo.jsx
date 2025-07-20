e449bf00bd492d3863c736be16df8543c625b32b
Ariel Monroy <ariel.monroy@jala.university>
2025-07-20 18:03:20 -0400

    feat: rename PokemonInfo component to PokemonInfoPage and refactor tab management

--- src/components/pages/PokemonInfo.jsx
+++ src/components/pages/PokemonInfo.jsx
@@ -1,24 +1,36 @@
-import { useParams } from "react-router";
-import PokemonTitle from "../molecules/PokemonTitle";
-import usePokemonInfo from "../../hooks/usePokemonInfo";
-import PokemonLoader from "../atoms/PokeballLoader";
-import PokemonStats from "../molecules/PokemonStats.jsx";
-import TabPanel from "../templates/TabPanel.jsx";
+import { useParams } from 'react-router';
+import PokemonTitle from '../molecules/PokemonTitle';
+import usePokemonInfo from '../../hooks/usePokemonInfo';
+import PokemonLoader from '../atoms/PokeballLoader';
+import PokemonStats from '../molecules/PokemonStats.jsx';
+import TabPanel from '../templates/TabPanel.jsx';
+import TypeBadge from '../molecules/TypeBadge.jsx';
+import PokemonInfo from '../molecules/PokemonInfo.jsx';
 
-export default function PokemonInfo() {
+export default function PokemonInfoPage() {
   const params = useParams();
   const { pokemon, loading, error } = usePokemonInfo({ id: params.id });
-  console.log("PokemonInfo", pokemon, loading, error);
+  const tabs = [
+    {
+      name: 'Stats',
+      component: props => <PokemonStats {...props} />,
+      props: { pokemon },
+    },
+    {
+      name: 'Title',
+      component: props => <PokemonTitle {...props} />,
+      props: { pokemon },
+    },
+  ];
   return (
     <div className="pokemon-info">
       <h1>Pokemon Information</h1>
       {loading && <PokemonLoader />}
       {error && <p className="error">Error: {error}</p>}
       {pokemon && (
-          <>
-            <PokemonTitle pokemon={pokemon} />
-            <TabPanel components={[ <PokemonStats pokemon={pokemon} />, <PokemonTitle pokemon={pokemon} /> ]} names={["Stats", "Title"]} />
-          </>
+        <>
+          <PokemonInfo pokemon={pokemon} />
+        </>
       )}
     </div>
   );
