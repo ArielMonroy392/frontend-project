fc15399b04ec5b14e1fe62069c20325241d0f93e
Ariel Monroy <ariel.monroy@jala.university>
2025-07-21 17:29:10 -0400

    feat: implement PokemonInfo component with styling and structure for displaying Pokémon details

--- src/components/molecules/PokemonInfo.jsx
+++ src/components/molecules/PokemonInfo.jsx
@@ -1,15 +1,25 @@
 import PokemonTitle from './PokemonTitle.jsx';
 import TypeBadge from './TypeBadge.jsx';
 import Text from '../atoms/Text.jsx';
+import './PokemonInfo.css';
+import { useNavigate } from 'react-router';
+import Button from '../atoms/Button.jsx';
+import Icon from '../atoms/Icon.jsx';
+import PokemonStats from './PokemonStats.jsx';
+import PokemonData from './PokemonData.jsx';
 
 export default function PokemonInfo({ pokemon }) {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="pokemon-info">
+      <Button onClick={() => navigate(-1)} variant='secondary'>
+        <Icon icon={'chevron_right'} size={24} />
+      </Button>
       <PokemonTitle pokemon={pokemon} size={'large'} color={'black'} />
       <Text className={'text-gray text-xxl font-bold'}>
         Generation {pokemon.generation}
       </Text>
-      <ul>
+      <ul className='type_list'>
         {pokemon.types.map((type, index) => (
           <li key={index}>
             <TypeBadge
@@ -22,6 +32,9 @@ export default function PokemonInfo({ pokemon }) {
           </li>
         ))}
       </ul>
+
+      <PokemonStats pokemon={pokemon} />
+      <PokemonData pokemon={pokemon} />
     </div>
   );
 }
