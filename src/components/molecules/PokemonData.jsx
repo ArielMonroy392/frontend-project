54eb389fc94892f6df135ff5ef5a90acfe49782d
Ariel Monroy <ariel.monroy@jala.university>
2025-07-21 17:28:57 -0400

    feat: add PokemonData component with styling and display of Pokémon details

---
+++ src/components/molecules/PokemonData.jsx
@@ -0,0 +1,38 @@
+import Text from "../atoms/Text";
+import TypeBadge from "./TypeBadge";
+import "./PokemonData.css";
+
+export default function PokemonData({ pokemon }) {
+  return (
+    <div className="pokemon-data">
+      <Text className={"text-black capitalize font-bold"}> About this pokemon</Text>
+      <p>{pokemon.flavor_text}</p>
+      <ul className="pokemon-data-list">
+        <li>
+          <Text className={"text-black capitalize font-bold"}>Species: </Text> {pokemon.species}
+        </li>
+        <li>
+          <Text className={"text-black capitalize font-bold"}>Height: </Text> {pokemon.height} m
+        </li>
+        <li>
+          <Text className={"text-black capitalize font-bold"}>Weight: </Text> {pokemon.weight} kg
+        </li>
+        <li>
+          <Text className={"text-black capitalize font-bold"}>Abilities: </Text> {pokemon.abilities.map((ability) => {
+            return <Text className={"text-black capitalize"} key={ability.name}>{ability.name} {ability.hidden ? '(Hidden)' : ''}</Text>;
+          })}
+        </li>
+        <li>
+          <Text className={"text-black capitalize font-bold"}>Weakness: </Text>
+          {pokemon.weaknesses.length > 0 ? (
+            pokemon.weaknesses.map((weakness, index) => (
+              <TypeBadge key={index} onlyIcon type={weakness} />
+            ))
+          ) : (
+            <Text className={"text-black capitalize"}>None</Text>
+          )}
+        </li>
+      </ul>
+    </div>
+  )
+}
