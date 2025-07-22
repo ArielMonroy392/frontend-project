6ee685ce7d3c9e34b6c834668e41f30276a464ec
Ariel Monroy <ariel.monroy@jala.university>
2025-07-22 17:25:37 -0400

    feat: enhance routing setup with logging for data fetching and lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -13,15 +13,28 @@ const router = createBrowserRouter([
     path: '/',
     Component: Layout,
     children: [
-      { index: true, Component: WhosThatPokemon },
+      {
+        index: true, Component: WhosThatPokemon,
+
+      },
       {
         path: "list",
         Component: List,
         loader: async () => {
+          console.log('Fetching initial data for List page');
           return {
             initialData: await fetchPokemons({ offset: 0, limit: 25 })
           }
         },
+        lazy: async () => {
+          console.log('Lazy loading List component');
+          const [List, loader] = await Promise.all([
+            import('./components/pages/List.jsx'),
+            import('./components/atoms/PokeballLoader.jsx')
+          ]);
+          return { default: List, loader: loader.default };
+
+        }
       },
       { path: "pokemon/:id", Component: PokemonInfo }
     ],
