e47c4aa2c4d813e661972d0ae29d8298ed2b286e
Ariel Monroy <ariel.monroy@jala.university>
2025-07-23 17:08:47 -0400

    feat: restructure PokemonInfo component layout and integrate PokemonImage

--- src/components/molecules/PokemonInfo.jsx
+++ src/components/molecules/PokemonInfo.jsx
@@ -2,16 +2,13 @@ import PokemonTitle from './PokemonTitle.jsx';
 import TypeBadge from './TypeBadge.jsx';
 import Text from '../atoms/Text.jsx';
 import './PokemonInfo.css';
-import { useNavigate } from 'react-router';
-import Button from '../atoms/Button.jsx';
-import Icon from '../atoms/Icon.jsx';
 import PokemonStats from './PokemonStats.jsx';
 import PokemonData from './PokemonData.jsx';
+import PokemonImage from '../atoms/PokemonImage.jsx';
 import TabPanel from '../templates/TabPanel.jsx';
 import { useMemo } from 'react';
 
 export default function PokemonInfo({ pokemon }) {
-  const navigate = useNavigate();
 
   const tabs = useMemo(() => [
     {
@@ -27,29 +24,34 @@ export default function PokemonInfo({ pokemon }) {
   ], [pokemon]);
 
   return (
-    <div className="pokemon-info">
-      <Button onClick={() => navigate(-1)} variant='secondary'>
-        <Icon icon={'chevron_right'} size={24} />
-      </Button>
-      <PokemonTitle pokemon={pokemon} size={'large'} color={'black'} />
-      <Text className={'text-gray text-xxl font-bold'}>
-        Generation {pokemon.generation}
-      </Text>
-      <ul className='type_list'>
-        {pokemon.types.map((type, index) => (
-          <li key={index}>
-            <TypeBadge
-              className={`badge ${type}`}
-              type={type}
-              onlyIcon
-              size={24}
-              rounded={'large'}
-            />
-          </li>
-        ))}
-      </ul>
-
-      <TabPanel tabs={tabs} />
+    <div className='info-container'>
+      <div className="pokemon-info">
+        <PokemonTitle pokemon={pokemon} size={'large'} color={'black'} />
+        <Text className={'text-gray text-xxl font-bold'}>
+          Generation {pokemon.generation}
+        </Text>
+        <ul className='type_list'>
+          {pokemon.types.map((type, index) => (
+            <li key={index}>
+              <TypeBadge
+                className={`badge ${type}`}
+                type={type}
+                onlyIcon
+                size={24}
+                rounded={'large'}
+              />
+            </li>
+          ))}
+        </ul>
+        <TabPanel tabs={tabs} />
+      </div>
+      <div className={`pokemon-image bg-${pokemon.types[0]}`}>
+        <div className="bg">
+          <img src="/dots-lg.svg" alt="Dots" className="dots" />
+          <img src="/pokeball-lg.svg" alt="Pokeball" className="pokeball" />
+        </div>
+        <PokemonImage src={pokemon.sprites.official} alt={`${pokemon.name} official artwork`} />
+      </div>
     </div>
   );
 }
