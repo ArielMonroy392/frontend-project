ed948bf41f7f4438304494ad2247bc39c85383aa
Ariel Monroy <ariel.monroy@jala.university>
2025-07-25 16:41:49 -0400

    feat: update PokemonInfo component with navigation buttons and background integration

--- src/components/molecules/PokemonInfo.jsx
+++ src/components/molecules/PokemonInfo.jsx
@@ -7,30 +7,40 @@ import PokemonData from './PokemonData.jsx';
 import PokemonImage from '../atoms/PokemonImage.jsx';
 import TabPanel from '../templates/TabPanel.jsx';
 import { useMemo } from 'react';
+import Background from '../atoms/Background.jsx';
+import Button from '../atoms/Button.jsx';
+import { useNavigate } from 'react-router';
+import Icon from '../atoms/Icon.jsx';
 
-export default function PokemonInfo({ pokemon }) {
-
-  const tabs = useMemo(() => [
-    {
-      name: 'Stats',
-      component: PokemonStats,
-      props: { pokemon },
-    },
-    {
-      name: 'Data',
-      component: PokemonData,
-      props: { pokemon },
-    },
-  ], [pokemon]);
+export default function PokemonInfo({ pokemon, previousPokemon, nextPokemon }) {
+  const navigate = useNavigate();
+  const tabs = useMemo(
+    () => [
+      {
+        name: 'Stats',
+        component: PokemonStats,
+        props: { pokemon },
+      },
+      {
+        name: 'Data',
+        component: PokemonData,
+        props: { pokemon },
+      },
+    ],
+    [pokemon]
+  );
 
   return (
-    <div className='info-container'>
+    <div className="info-container">
       <div className="pokemon-info">
+        <Button onClick={() => navigate(-1)} variant="secondary">
+          <Icon icon={'chevron_right'} size={24} />
+        </Button>
         <PokemonTitle pokemon={pokemon} size={'large'} color={'black'} />
         <Text className={'text-gray text-xxl font-bold'}>
           Generation {pokemon.generation}
         </Text>
-        <ul className='type_list'>
+        <ul className="type_list">
           {pokemon.types.map((type, index) => (
             <li key={index}>
               <TypeBadge
@@ -46,11 +56,17 @@ export default function PokemonInfo({ pokemon }) {
         <TabPanel tabs={tabs} />
       </div>
       <div className={`pokemon-image bg-${pokemon.types[0]}`}>
-        <div className="bg">
-          <img src="/dots-lg.svg" alt="Dots" className="dots" />
-          <img src="/pokeball-lg.svg" alt="Pokeball" className="pokeball" />
-        </div>
-        <PokemonImage src={pokemon.sprites.official} alt={`${pokemon.name} official artwork`} />
+        <Background size={'lg'} />
+        <PokemonImage
+          src={pokemon.sprites.official}
+          alt={`${pokemon.name} official artwork`}
+        />
+        <Button onClick={previousPokemon}>
+          Previous #{(pokemon.id - 1).toString().padStart(3, '0')}
+        </Button>
+        <Button onClick={nextPokemon}>
+          Next #{(pokemon.id + 1).toString().padStart(3, '0')}
+        </Button>
       </div>
     </div>
   );
