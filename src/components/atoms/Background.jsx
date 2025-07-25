e2ce0175227de71104d7a9a3b3eab65d7a258aea
Ariel Monroy <ariel.monroy@jala.university>
2025-07-25 17:29:44 -0400

    fix: replace fragment with div in Background component for proper structure

--- src/components/atoms/Background.jsx
+++ src/components/atoms/Background.jsx
@@ -1,8 +1,8 @@
-export default function Background({ size = 'md'}) {
+export default function Background({ size = 'md' }) {
     return (
-        <>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
             <img src="/dots-lg.svg" alt="Dots" className="dots" />
             <img src="/pokeball-lg.svg" alt="Pokeball" className="pokeball" />
-        </>
+        </div>
     )
 }
