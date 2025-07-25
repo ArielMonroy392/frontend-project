5b3a289497c6408c9a6cbb1d8e291787926909e3
Ariel Monroy <ariel.monroy@jala.university>
2025-07-25 16:40:25 -0400

    feat: add Background component with styled images for dots and pokeball

---
+++ src/components/atoms/Background.jsx
@@ -0,0 +1,8 @@
+export default function Background({ size = 'md'}) {
+    return (
+        <>
+            <img src="/dots-lg.svg" alt="Dots" className="dots" />
+            <img src="/pokeball-lg.svg" alt="Pokeball" className="pokeball" />
+        </>
+    )
+}
