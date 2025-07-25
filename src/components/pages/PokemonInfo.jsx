d654d2033ca9557ac8a6603ddd6b35c7167d902d
Ariel Monroy <ariel.monroy@jala.university>
2025-07-25 16:41:11 -0400

    feat: enhance PokemonInfoPage with navigation buttons for previous and next Pokémon

--- src/components/pages/PokemonInfo.jsx
+++ src/components/pages/PokemonInfo.jsx
@@ -3,23 +3,37 @@ import usePokemonInfo from '../../hooks/usePokemonInfo';
 import PokemonLoader from '../atoms/PokeballLoader';
 import PokemonInfo from '../molecules/PokemonInfo.jsx';
 import { useNavigate } from 'react-router';
-import Button from "../atoms/Button.jsx";
+import Button from '../atoms/Button.jsx';
 import Icon from '../atoms/Icon.jsx';
+import { useCallback } from 'react';
 
 export default function PokemonInfoPage() {
   const params = useParams();
   const { pokemon, loading, error } = usePokemonInfo({ id: params.id });
   const navigate = useNavigate();
+  const previousPokemon = useCallback(async () => {
+    if (pokemon && pokemon.id > 1) {
+      navigate(`/pokemon/${pokemon.id - 1}`);
+    }
+  }, [pokemon, navigate]);
+
+  const nextPokemon = useCallback(async () => {
+    if (pokemon && pokemon.id < 898) {
+      navigate(`/pokemon/${pokemon.id + 1}`);
+    }
+  }, [pokemon, navigate]);
+
   return (
     <div className="pokemon-info">
-      <Button onClick={() => navigate(-1)} variant='secondary'>
-        <Icon icon={'chevron_right'} size={24} />
-      </Button>
       {loading && <PokemonLoader />}
       {error && <p className="error">Error: {error}</p>}
       {pokemon && (
         <>
-          <PokemonInfo pokemon={pokemon} />
+          <PokemonInfo
+            pokemon={pokemon}
+            nextPokemon={nextPokemon}
+            previousPokemon={previousPokemon}
+          />
         </>
       )}
     </div>
