2f2003419079a02020696809c8ee363f6ec74ab2
Ariel Monroy <ariel.monroy@jala.university>
2025-07-25 16:42:43 -0400

    feat: added max, min and statBar for pokemon stats

--- src/components/molecules/PokemonStats.jsx
+++ src/components/molecules/PokemonStats.jsx
@@ -1,16 +1,31 @@
-import Text from "../atoms/Text";
-import "./PokemonStats.css";
+import Text from '../atoms/Text';
+import './PokemonStats.css';
 
 export default function PokemonStats({ pokemon }) {
   if (!pokemon) return null;
 
   return (
     <div className="pokemon-stats">
-      <Text className={`text-${pokemon.types[0]} capitalize font-bold`}>Base Stats</Text>
+      <Text className={`text-${pokemon.types[0]} capitalize font-bold`}>
+        Base Stats
+      </Text>
       <ul className="pokemon-stats-list">
-        {pokemon.stats.map((stat) => (
+        {pokemon.stats.map(stat => (
           <li key={stat.name}>
-            <Text className={`text-black capitalize font-bold`}>{stat.name}</Text>: {stat.base}
+            <Text
+              className={`text-black capitalize font-bold`}
+              style={{ width: '10rem' }}
+            >
+              {stat.name}
+            </Text>
+            <Text>{stat.base}</Text>
+            <span style={{ width: '5rem' }}>
+              {stat.name !== 'total' && (
+                <progress value={stat.base} max={255} />
+              )}
+            </span>
+            <Text> {stat.min}</Text>
+            <Text> {stat.max}</Text>
           </li>
         ))}
       </ul>
