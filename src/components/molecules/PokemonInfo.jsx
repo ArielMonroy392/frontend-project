e2ab4fde2c9755897ee5e1f685529c31437b0ebb
Ariel Monroy <ariel.monroy@jala.university>
2025-07-26 12:59:47 -0400

    feat: enhance PokemonInfo layout and styling for improved user experience

--- src/components/molecules/PokemonInfo.jsx
+++ src/components/molecules/PokemonInfo.jsx
@@ -2,8 +2,7 @@ import PokemonTitle from './PokemonTitle.jsx';
 import TypeBadge from './TypeBadge.jsx';
 import Text from '../atoms/Text.jsx';
 import './PokemonInfo.css';
-import PokemonStats from './PokemonStats.jsx';
-import PokemonData from './PokemonData.jsx';
+
 import PokemonImage from '../atoms/PokemonImage.jsx';
 import TabPanel from '../templates/TabPanel.jsx';
 import { useMemo } from 'react';
@@ -13,61 +12,8 @@ import { useNavigate } from 'react-router';
 import Icon from '../atoms/Icon.jsx';
 
 export default function PokemonInfo({ pokemon, previousPokemon, nextPokemon }) {
-  const navigate = useNavigate();
-  const tabs = useMemo(
-    () => [
-      {
-        name: 'Stats',
-        component: PokemonStats,
-        props: { pokemon },
-      },
-      {
-        name: 'Data',
-        component: PokemonData,
-        props: { pokemon },
-      },
-    ],
-    [pokemon]
-  );
 
   return (
-    <div className="info-container">
-      <div className="pokemon-info">
-        <Button onClick={() => navigate(-1)} variant="secondary">
-          <Icon icon={'chevron_right'} size={24} />
-        </Button>
-        <PokemonTitle pokemon={pokemon} size={'large'} color={'black'} />
-        <Text className={'text-gray text-xxl font-bold'}>
-          Generation {pokemon.generation}
-        </Text>
-        <ul className="type_list">
-          {pokemon.types.map((type, index) => (
-            <li key={index}>
-              <TypeBadge
-                className={`badge ${type}`}
-                type={type}
-                onlyIcon
-                size={24}
-                rounded={'large'}
-              />
-            </li>
-          ))}
-        </ul>
-        <TabPanel tabs={tabs} />
-      </div>
-      <div className={`pokemon-image bg-${pokemon.types[0]}`}>
-        <Background size={'lg'} />
-        <PokemonImage
-          src={pokemon.sprites.official}
-          alt={`${pokemon.name} official artwork`}
-        />
-        <Button onClick={previousPokemon}>
-          Previous #{(pokemon.id - 1).toString().padStart(3, '0')}
-        </Button>
-        <Button onClick={nextPokemon}>
-          Next #{(pokemon.id + 1).toString().padStart(3, '0')}
-        </Button>
-      </div>
-    </div>
+    
   );
 }
--- src/components/pages/PokemonInfo.jsx
+++ src/components/pages/PokemonInfo.jsx
@@ -1,14 +1,42 @@
 import { useParams } from 'react-router';
 import usePokemonInfo from '../../hooks/usePokemonInfo';
 import PokemonLoader from '../atoms/PokeballLoader';
-import PokemonInfo from '../molecules/PokemonInfo.jsx';
 import { useNavigate } from 'react-router';
 import { useCallback } from 'react';
+import "./PokemonInfo.css"
+import { useMemo } from 'react';
+import PokemonStats from '../molecules/PokemonStats.jsx';
+import PokemonData from '../molecules/PokemonData.jsx';
+import './PokemonInfo.css';
+import Button from '../atoms/Button.jsx';
+import Icon from '../atoms/Icon';
+import PokemonTitle from '../molecules/PokemonTitle.jsx';
+import TypeBadge from '../molecules/TypeBadge.jsx';
+import TabPanel from '../templates/TabPanel.jsx';
+import Background from '../atoms/Background.jsx';
+import PokemonImage from '../atoms/PokemonImage';
+import Text from '../atoms/Text.jsx';
 
 export default function PokemonInfoPage() {
   const params = useParams();
   const { pokemon, loading, error } = usePokemonInfo({ id: params.id });
   const navigate = useNavigate();
+  const tabs = useMemo(
+    () => [
+      {
+        name: 'Stats',
+        component: PokemonStats,
+        props: { pokemon },
+      },
+      {
+        name: 'Data',
+        component: PokemonData,
+        props: { pokemon },
+      },
+    ],
+    [pokemon]
+  );
+
   const previousPokemon = useCallback(async () => {
     if (pokemon && pokemon.id > 1) {
       navigate(`/pokemon/${pokemon.id - 1}`);
@@ -22,18 +50,53 @@ export default function PokemonInfoPage() {
   }, [pokemon, navigate]);
 
   return (
-    <div className="pokemon-info">
+    <>
       {loading && <PokemonLoader />}
       {error && <p className="error">Error: {error}</p>}
       {pokemon && (
         <>
-          <PokemonInfo
-            pokemon={pokemon}
-            nextPokemon={nextPokemon}
-            previousPokemon={previousPokemon}
-          />
+          <div className="info-container">
+            <div className="pokemon-info">
+              <Button onClick={() => navigate(-1)} variant="secondary">
+                <Icon icon={'chevron_right'} size={24} />
+              </Button>
+              <PokemonTitle pokemon={pokemon} size={'large'} color={'black'} />
+              <Text className={'text-gray text-xxl font-bold'}>
+                Generation {pokemon.generation}
+              </Text>
+              <ul className="type_list">
+                {pokemon.types.map((type, index) => (
+                  <li key={index}>
+                    <TypeBadge
+                      className={`badge ${type}`}
+                      type={type}
+                      onlyIcon
+                      size={24}
+                      rounded={'large'}
+                    />
+                  </li>
+                ))}
+              </ul>
+              <TabPanel tabs={tabs} />
+            </div>
+            <div className={`pokemon-image bg-${pokemon.types[0]}`}>
+              <Background size={'lg'} />
+              <PokemonImage
+                src={pokemon.sprites.official}
+                alt={`${pokemon.name} official artwork`}
+              />
+              <div style={{ display: 'flex', gap: '1rem', padding: '2rem' }}>
+                <Button onClick={previousPokemon} variant='secondary'>
+                  Previous #{(pokemon.id - 1).toString().padStart(3, '0')}
+                </Button>
+                <Button onClick={nextPokemon}>
+                  Next #{(pokemon.id + 1).toString().padStart(3, '0')}
+                </Button>
+              </div>
+            </div>
+          </div>
         </>
       )}
-    </div>
+    </>
   );
 }
