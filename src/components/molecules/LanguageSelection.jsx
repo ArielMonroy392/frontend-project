6a3df2d1c84f641407e16fa06da007fbd998bf6b
Ariel Monroy <ariel.monroy@jala.university>
2025-07-27 17:14:38 -0400

    refactor: rename LanguageSelection component files and update class names for consistency

--- src/components/molecules/LanguageSelection.jsx
+++ src/components/molecules/LanguageSelection.jsx
@@ -1,13 +1,13 @@
-import FlagButton from '../atoms/FlagButton';
+import FlagButton from '../atoms/FlagButton.jsx';
 import styles from './LanguageSelection.module.css';
-import Text from '../atoms/Text';
-import { SUPPORTED_LANGUAGES } from '../../constants/game';
+import Text from '../atoms/Text.jsx';
+import { SUPPORTED_LANGUAGES } from '../../constants/game.js';
 
 export default function LanguageSelection({ onLanguageChange }) {
   return (
     <div className={styles.languageSelection}>
-      <Text className={'text-large text-white font-bold'}>Select Language</Text>
-      <ul className="language-list">
+      <Text className={'textLarge textWhite fontBold'}>Select Language</Text>
+      <ul className={styles.languageList}>
         {SUPPORTED_LANGUAGES.map(language => (
           <li key={language}>
             <FlagButton
