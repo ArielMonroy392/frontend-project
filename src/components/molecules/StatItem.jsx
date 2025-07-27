7957f2f170530cc11325b69ca8dce5d3d356426f
Ariel Monroy <ariel.monroy@jala.university>
2025-07-27 18:55:50 -0400

    refactor: wrap stat title in a span and add progress bar styles

--- src/components/molecules/StatItem.jsx
+++ src/components/molecules/StatItem.jsx
@@ -4,12 +4,14 @@ import Text from '../atoms/Text.jsx';
 export default function StatItem({ stat }) {
   return (
     <li className={styles.statItem}>
-      <Text className={`text-black capitalize font-bold ${styles.title}`}>
-        {stat.name}
-      </Text>
+      <span className={styles.title}>
+        <Text className={`text-black capitalize font-bold`}>{stat.name}</Text>
+      </span>
       <Text>{stat.base}</Text>
       <span style={{ width: '5rem' }}>
-        {stat.name !== 'total' && <progress value={stat.base} max={255} />}
+        {stat.name !== 'total' && (
+          <progress className={styles.bar} value={stat.base} max={255} />
+        )}
       </span>
       <Text> {stat.min}</Text>
       <Text> {stat.max}</Text>
