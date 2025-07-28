d301f1cf7b77fbe05af29eb1b92b177ef021127e
Ariel Monroy <ariel.monroy@jala.university>
2025-07-27 20:16:43 -0400

    feat: add PokemonHeaderSection component with styling and layout

---
+++ src/components/molecules/PokemonHeaderSection.jsx
@@ -0,0 +1,33 @@
+import Button from '../atoms/Button';
+import Icon from '../atoms/Icon';
+import PokemonIdentity from './PokemonIdentity';
+import Text from '../atoms/Text';
+import TypeBadge from './TypeBadge';
+import styles from './PokemonHeaderSection.module.css';
+
+export default function PokemonHeaderSection({ pokemon, onBack }) {
+  return (
+    <div className={styles.headerContainer}>
+      <Button onClick={onBack} variant="secondary">
+        <Icon icon={'chevron_right'} size={24} />
+      </Button>
+      <PokemonIdentity pokemon={pokemon} size={'large'} color={'black'} />
+      <Text className="textGray textXxl fontBold">
+        Generation {pokemon.generation}
+      </Text>
+      <ul className={styles.typeList}>
+        {pokemon.types.map((type, i) => (
+          <li key={i}>
+            <TypeBadge
+              className={`badge ${type}`}
+              type={type}
+              onlyIcon
+              size={24}
+              rounded="large"
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
