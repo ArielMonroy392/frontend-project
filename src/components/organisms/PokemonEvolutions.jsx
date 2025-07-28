bf36103501bd805ec717733b127b18013f75c6e1
Ariel Monroy <ariel.monroy@jala.university>
2025-07-27 21:57:33 -0400

    feat: add PokemonEvolutions component to display evolution chart

---
+++ src/components/organisms/PokemonEvolutions.jsx
@@ -0,0 +1,19 @@
+import PokemonEvolutionTile from '../molecules/PokemonEvolutionTile.jsx';
+import Text from '../atoms/Text.jsx';
+import styles from './PokemonEvolutions.module.css';
+
+export default function PokemonEvolutions({ pokemon }) {
+  console.log(pokemon.evolution);
+  if (!pokemon || !pokemon.evolution) return null;
+
+  return (
+    <div className={styles.pokemonEvolutions}>
+      <Text className="textBlack capitalize fontBold">Evolution Chart</Text>
+      <ul className={styles.pokemonEvolutionsList}>
+        {pokemon.evolution.chain.map((evolution, index) => (
+          <PokemonEvolutionTile evolution={evolution} key={index} />
+        ))}
+      </ul>
+    </div>
+  );
+}
