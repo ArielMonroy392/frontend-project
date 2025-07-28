731ef77d66fb7f453bb68d913b276e404725a03d
Ariel Monroy <ariel.monroy@jala.university>
2025-07-27 21:57:45 -0400

    fix: update StatItem styles for improved consistency

--- src/components/molecules/StatItem.jsx
+++ src/components/molecules/StatItem.jsx
@@ -5,7 +5,7 @@ export default function StatItem({ stat }) {
   return (
     <li className={styles.statItem}>
       <span className={styles.title}>
-        <Text className={`text-black capitalize font-bold`}>{stat.name}</Text>
+        <Text className={`textBlack capitalize fontBold`}>{stat.name}</Text>
       </span>
       <Text>{stat.base}</Text>
       <span style={{ width: '5rem' }}>
